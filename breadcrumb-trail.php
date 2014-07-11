39a520dbef9ea26a8292cf36cd3cb70fecb3b6d8
Justin Tadlock <justin@justintadlock.com>
2014-07-11 10:59:42 -0500

    Version bump to 0.6.1.

--- breadcrumb-trail.php
+++ breadcrumb-trail.php
@@ -3,7 +3,7 @@
  * Plugin Name: Breadcrumb Trail
  * Plugin URI:  http://themehybrid.com/plugins/breadcrumb-trail
  * Description: A smart breadcrumb menu plugin embedded with <a href="http://schema.org">Schema.org</a> microdata that can handle variations in site structure more accurately than any other breadcrumb plugin for WordPress. Insert into your theme with the <code>breadcrumb_trail()</code> template tag.
- * Version:     0.6.1-alpha
+ * Version:     0.6.1
  * Author:      Justin Tadlock
  * Author URI:  http://justintadlock.com
  * Text Domain: breadcrumb-trail
