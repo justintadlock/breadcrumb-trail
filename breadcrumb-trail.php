8a1863707d34999d2279146a69ad7c4e0d5ce0ea
Justin Tadlock <justin@justintadlock.com>
2014-07-11 10:59:17 -0500

    Clean up plugin header.

--- breadcrumb-trail.php
+++ breadcrumb-trail.php
@@ -1,12 +1,12 @@
 <?php
 /**
  * Plugin Name: Breadcrumb Trail
- * Plugin URI: http://themehybrid.com/plugins/breadcrumb-trail
+ * Plugin URI:  http://themehybrid.com/plugins/breadcrumb-trail
  * Description: A smart breadcrumb menu plugin embedded with <a href="http://schema.org">Schema.org</a> microdata that can handle variations in site structure more accurately than any other breadcrumb plugin for WordPress. Insert into your theme with the <code>breadcrumb_trail()</code> template tag.
- * Version: 0.6.1-alpha
- * Author: Justin Tadlock
- * Author URI: http://justintadlock.com
- * Text Domain: cleaner-gallery
+ * Version:     0.6.1-alpha
+ * Author:      Justin Tadlock
+ * Author URI:  http://justintadlock.com
+ * Text Domain: breadcrumb-trail
  * Domain Path: /languages
  */
 
