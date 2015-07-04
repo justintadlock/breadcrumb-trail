70c9381dac3e9b8f10c0f8e4671a48b8f1e71f2f
Justin Tadlock <justin@justintadlock.com>
2015-07-04 12:14:27 -0500

    Move trail() method to after default_labels() method.

--- inc/breadcrumbs.php
+++ inc/breadcrumbs.php
@@ -111,6 +111,40 @@ class Breadcrumb_Trail {
 	}
 
 	/**
+	 * Returns an array of the default labels.
+	 *
+	 * @since  0.6.0
+	 * @access public
+	 * @return array
+	 */
+	public function default_labels() {
+
+		$labels = array(
+			'browse'              => esc_html__( 'Browse:',                             'breadcrumb-trail' ),
+			'home'                => esc_html__( 'Home',                                'breadcrumb-trail' ),
+			'error_404'           => esc_html__( '404 Not Found',                       'breadcrumb-trail' ),
+			'archives'            => esc_html__( 'Archives',                            'breadcrumb-trail' ),
+			// Translators: %s is the search query. The HTML entities are opening and closing curly quotes.
+			'search'              => esc_html__( 'Search results for &#8220;%s&#8221;', 'breadcrumb-trail' ),
+			// Translators: %s is the page number.
+			'paged'               => esc_html__( 'Page %s',                             'breadcrumb-trail' ),
+			// Translators: Minute archive title. %s is the minute time format.
+			'archive_minute'      => esc_html__( 'Minute %s',                           'breadcrumb-trail' ),
+			// Translators: Weekly archive title. %s is the week date format.
+			'archive_week'        => esc_html__( 'Week %s',                             'breadcrumb-trail' ),
+
+			// "%s" is replaced with the translated date/time format.
+			'archive_minute_hour' => '%s',
+			'archive_hour'        => '%s',
+			'archive_day'         => '%s',
+			'archive_month'       => '%s',
+			'archive_year'        => '%s',
+		);
+
+		return $labels;
+	}
+
+	/**
 	 * Formats and outputs the breadcrumb trail.
 	 *
 	 * @since  0.6.0
@@ -201,40 +235,6 @@ class Breadcrumb_Trail {
 			echo $breadcrumb;
 		else
 			return $breadcrumb;
-	}
-
-	/**
-	 * Returns an array of the default labels.
-	 *
-	 * @since  0.6.0
-	 * @access public
-	 * @return array
-	 */
-	public function default_labels() {
-
-		$labels = array(
-			'browse'              => esc_html__( 'Browse:',                             'breadcrumb-trail' ),
-			'home'                => esc_html__( 'Home',                                'breadcrumb-trail' ),
-			'error_404'           => esc_html__( '404 Not Found',                       'breadcrumb-trail' ),
-			'archives'            => esc_html__( 'Archives',                            'breadcrumb-trail' ),
-			// Translators: %s is the search query. The HTML entities are opening and closing curly quotes.
-			'search'              => esc_html__( 'Search results for &#8220;%s&#8221;', 'breadcrumb-trail' ),
-			// Translators: %s is the page number.
-			'paged'               => esc_html__( 'Page %s',                             'breadcrumb-trail' ),
-			// Translators: Minute archive title. %s is the minute time format.
-			'archive_minute'      => esc_html__( 'Minute %s',                           'breadcrumb-trail' ),
-			// Translators: Weekly archive title. %s is the week date format.
-			'archive_week'        => esc_html__( 'Week %s',                             'breadcrumb-trail' ),
-
-			// "%s" is replaced with the translated date/time format.
-			'archive_minute_hour' => '%s',
-			'archive_hour'        => '%s',
-			'archive_day'         => '%s',
-			'archive_month'       => '%s',
-			'archive_year'        => '%s',
-		);
-
-		return $labels;
 	}
 
 	/**
